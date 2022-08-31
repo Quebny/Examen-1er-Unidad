a934f044ade7b97b0d9a277940942a8d980e1f41
Quebny <quebynliga@gmail.com>
2022-08-31 03:11:43 -0500

    ataques y sistema de animaciones revamp (WIP) + sonidos y sprites

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cv = null;
 var ctx = null;
 var player = null;
+var enemy = null;
 var super_x = 240, super_y = 240;
 var score = 0;
 var pause = false;
@@ -14,53 +15,95 @@ var height = 800;
 
 var direction = "right";
 var moving = false;
+var attacking = false;
+var finalFrame;
 
-//animations
-var frameWidth = 64;
-var frameHeight = 64;
-var scale = 2;
-var fps = 60;
-var secondsToUpdate = 1 * fps;
-var frameCount = 0;
-var frameID = 0;
+//audio
+var bgm = new Audio('sound/bgm.mp3');
+var hit = new Audio('sound/hit.wav');
+var swing = new Audio('sound/swing.wav');
+
+//Entities
+var Entity = {
+    frameData: {},
+    createEntity: function (name, frameWidth, frameHeight, scale, frameCount, frameID) {
+        if (!this.frameData[name]) {
+            this.frameData[name] = {
+                frameWidth: frameWidth,
+                frameHeight: frameHeight,
+                scale: scale,
+                frameCount: frameCount,
+                frameID: frameID
+            }
+        }
+
+    },
+    getEntity: function (name) {
+        if (this.frameData[name]) {
+            return this.frameData[name];
+        }
+    }
+}
+
+// var frameWidth = 64;
+// var frameHeight = 64;
+// var scale = 2;
+// var frameCount = 0;
+// var frameID = 0;
 
 //animation states
 var State = {
     states: {},
-    generateState: function (name, startID, endID) {
-        if (!this.states[name]) {
-            this.states[name] = {
+    generateState: function (entityN, name, startID, endID) {
+        if (!this.states[entityN]) {
+            this.states[entityN] = {
+                name: name,
                 frameID: startID,
                 startID: startID,
                 endID: endID
             }
         }
     },
-    getState: function (name) {
-        if (this.states[name]) {
-            return this.states[name];
+    getState: function (entityN) {
+        if (this.states[entityN]) {
+            return this.states[entityN];
         }
+    },
+    setState: function (newState) {
+        this.states[name] = newState;
     }
 }
-//movement states
-State.generateState("move_right", 0, 5);
-State.generateState("move_up", 6, 11);
-State.generateState("move_left", 12, 17);
-State.generateState("move_down", 18, 23);
 
-State.generateState("idle_right", 0, 0);
-State.generateState("idle_up", 6, 6);
-State.generateState("idle_left", 12, 12);
-State.generateState("idle_down", 18, 18);
+
+//Entities
+Entity.createEntity("player", 64, 64, 2, 0, 0);
+Entity.createEntity("enemy", 16, 24, 2, 0, 0);
+
+//movement states
+State.generateState("player", "move_right", 0, 5);
+State.generateState("player", "move_up", 6, 11);
+State.generateState("player", "move_left", 12, 17);
+State.generateState("player", "move_down", 18, 23);
+
+State.generateState("player", "idle_right", 0, 0);
+State.generateState("player", "idle_up", 6, 6);
+State.generateState("player", "idle_left", 12, 12);
+State.generateState("player", "idle_down", 18, 18);
 
 //attack states
-State.generateState("attack_right", 0, 0);
-State.generateState("attack_up", 0, 0);
-State.generateState("attack_left", 0, 0);
-State.generateState("attack_down", 0, 0);
+State.generateState("player", "attack_right", 24, 27);
+State.generateState("player", "attack_up", 28, 31);
+State.generateState("player", "attack_left", 32, 35);
+State.generateState("player", "attack_down", 36, 39);
 
-var spriteSheet = new Image();
-spriteSheet.src = "sprites/Player.png";
+//enemy states
+State.generateState("enemy", "enemy_move", 0, 4)
+
+var playerSpriteSheet = new Image();
+playerSpriteSheet.src = "sprites/Player.png";
+
+var enemySpriteSheet = new Image();
+enemySpriteSheet.src = "sprites/Bat.png"
 
 
 function getRandomInt(min, max) {
@@ -69,12 +112,12 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 function start() {
     cv = document.getElementById('mycanvas');
     ctx = cv.getContext('2d')
 
     player = new box(super_x, super_y, 40, 40);
+    enemy = new box(750, 250, 30, 30);
 
     paint();
 }
@@ -83,7 +126,16 @@ function start() {
 document.addEventListener('keydown', function (e) {
     //movimiento
     move[e.keyCode] = true;
-    moving = true;
+    if (!attacking) {
+        if (move[68] || move[65] || move[87] || move[83]) {
+            moving = true;
+        }
+    }
+
+    if (move[74]) {
+        attacking = true;
+        swing.play()
+    }
 
     //pause
     if (e.keyCode == 32) {
@@ -106,31 +158,60 @@ function box(x, y, w, h) {
     this.h = h;
 
     this.dibujar = function (ctx) {
-        ctx.fillStyle = this.c;
         ctx.strokeRect(this.x, this.y, this.w, this.h);
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 }
 
-function animate(state) {
+function animate() {
+    State.setState("move_down")
+    // console.log(State.getState("player"));
+    console.log(Entity)
+
+    //player drawing
     ctx.drawImage(
-        spriteSheet,
-        state.frameID * frameWidth, 0,
-        frameWidth, frameHeight,
+        playerSpriteSheet,
+        State.getState("player").frameID * Entity.getEntity("player").frameWidth, 0,
+        Entity.getEntity("player").frameWidth, Entity.getEntity("player").frameHeight,
         player.x - 40, player.y - 45,
-        frameWidth * scale, frameHeight * scale
+        Entity.getEntity("player").frameWidth * Entity.getEntity("player").scale,
+        Entity.getEntity("player").frameHeight * Entity.getEntity("player").scale
     );
+
+    // //enemy drawing
+    // ctx.drawImage(
+    //     enemySpriteSheet,
+    //     state.getEntityStates["enemy"] * Entity.getEntity("enemy").frameWidth, 0,
+    //     Entity.getEntity("enemy").frameWidth, Entity.getEntity("enemy").frameHeight,
+    //     enemy.x - 2, enemy.y,
+    //     Entity.getEntity("enemy").frameWidth * Entity.getEntity("enemy").scale,
+    //     Entity.getEntity("enemy").frameHeight * Entity.getEntity("enemy").scale
+    // );
+
     //frame speed
-    frameCount++;
-    if (frameCount > 4) {
-        state.frameID++;
-        frameCount = 0;
+
+    Entity.getEntity("player").frameCount++;
+    if (Entity.getEntity("player").frameCount > 4) {
+        State.getState("player").frameID++;
+        Entity.getEntity("player").frameCount = 0;
     }
 
+    // Entity.getEntity("enemy").frameCount++;
+    // if (Entity.getEntity("enemy").frameCount > 4) {
+    //     state.getState[12].frameID++;
+    //     Entity.getEntity("enemy").frameCount = 0;
+    // }
+
     //frames used
-    if (state.frameID > state.endID) {
-        state.frameID = state.startID;
+    if (State.getState("player").frameID > State.getState("player").endID) {
+        State.getState("player").frameID = State.getState("player").startID;
+        attacking = false;
     }
+
+    // if (state.frameID > state.endID) {
+    //     state.frameID = state.startID;
+    // }
+
 }
 
 function paint() {
@@ -140,14 +221,19 @@ function paint() {
     ctx.fillRect(0, 0, width, height);
 
     //collision box (debug)
+    ctx.strokeStyle = "black";
     player.dibujar(ctx);
 
-    //player drawing
+    ctx.strokeStyle = "red";
+    enemy.dibujar(ctx);
+
 
 
     if (!pause) {
         update();
     } else {
+
+
         ctx.fillStyle = "rgba(0,0,0,0.5)"
         ctx.fillRect(0, 0, width, height);
 
@@ -159,48 +245,51 @@ function paint() {
 //player inputs
 function update() {
 
-    //derecha
-    if (move[68]) {
-        player.x += speed;
-        animate(State.getState("move_right"));
-        direction = 'right';
-        if (player.x > width) {
-            player.x = 0;
+    if (!attacking) { //detiene al jugador al atacar
+        //derecha
+        if (move[68]) {
+            player.x += speed;
+            animate(State.getState("player").name("move_right"));
+            direction = 'right';
+            if (player.x > width) {
+                player.x = 0;
+            }
+        }
+
+        //izquierda
+        if (move[65]) {
+            player.x -= speed;
+            animate(State.getState("move_left"));
+            direction = 'left';
+            if (player.x < 0) {
+                player.x = width;
+
+            }
+        }
+
+        //arriba
+        if (move[87]) {
+            player.y -= speed;
+            animate(State.getState("move_up"));
+            direction = 'up';
+            if (player.y < 0) {
+                player.y = height;
+            }
+        }
+
+        //abajo
+        if (move[83]) {
+            player.y += speed;
+            animate(State.getState("move_down"));
+            direction = 'down';
+            if (player.y > height) {
+                player.y = 0;
+            }
         }
     }
 
-    //izquierda
-    if (move[65]) {
-        player.x -= speed;
-        animate(State.getState("move_left"));
-        direction = 'left';
-        if (player.x < 0) {
-            player.x = width;
 
-        }
-    }
-
-    //arriba
-    if (move[87]) {
-        player.y -= speed;
-        animate(State.getState("move_up"));
-        direction = 'up';
-        if (player.y < 0) {
-            player.y = height;
-        }
-    }
-
-    //abajo
-    if (move[83]) {
-        player.y += speed;
-        animate(State.getState("move_down"));
-        direction = 'down';
-        if (player.y > height) {
-            player.y = 0;
-        }
-    }
-
-    if (!moving) {
+    if (!moving && !attacking) {
         if (direction == 'right') {
             animate(State.getState("idle_right"));
         }
@@ -212,6 +301,21 @@ function update() {
         }
         if (direction == 'left') {
             animate(State.getState("idle_left"));
+        }
+    }
+
+    if (attacking) {
+        if (direction == 'right') {
+            animate(State.getState("attack_right"));
+        }
+        if (direction == 'up') {
+            animate(State.getState("attack_up"));
+        }
+        if (direction == 'down') {
+            animate(State.getState("attack_down"));
+        }
+        if (direction == 'left') {
+            animate(State.getState("attack_left"));
         }
     }
 }
